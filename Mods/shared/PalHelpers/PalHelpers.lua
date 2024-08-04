73b5cd2933a23fed8e6f7848ef2d8e20a87a4e05
Renell Castro <renellc9819@gmail.com>
2024-08-03 22:57:18 -0700

    Added `PalStorageFilterer` class
    - Added `PalStorageFilterer` class which handles filtering for specific
      pals within a user's Pal storage.
    - Moved `pal_storage_ipairs` into new module `PalHelpers.palstorage`

--- Mods/shared/PalHelpers/PalHelpers.lua
+++ Mods/shared/PalHelpers/PalHelpers.lua
@@ -20,61 +20,6 @@ function PalHelpers.GetPalUIUtility()
     return pal_ui_util
 end
 
-PalHelpers["PAL_STORAGE_MAX_PAGES"] = 32
-PalHelpers["PAL_STORAGE_MAX_SLOTS_PER_PAGE"] = 30
-
----@param pal_storage UPalPlayerDataPalStorage
-function PalHelpers.pal_storage_ipairs(pal_storage)
-    local page_idx = 0
-    local slot_idx = 0
-
-    local function iterator()
-        if page_idx == PalHelpers["PAL_STORAGE_MAX_PAGES"] then
-            return nil
-        end
-
-        if slot_idx == PalHelpers["PAL_STORAGE_MAX_SLOTS_PER_PAGE"] then
-            page_idx = page_idx + 1
-            slot_idx = 0
-        end
-
-        -- In a user's Pal storage, there can be gaps between Pal storage slots. This means that the next slot in the
-        -- page might not necessarily mean there are no more Pals in the user's storage. Because of this, we must loop
-        -- through every slot in the user's storage to ensure we aren't missing any Pals that might've been placed
-        -- on a later page manually.
-        --
-        -- If there is a way to first sort a user's storage and ensure there are no gaps between storage slots, then
-        -- this should be refactored to loop through the storage until it reaches an empty slot.
-        local curr_page_idx = page_idx
-        local curr_slot_idx = slot_idx
-
-        while curr_page_idx < PalHelpers["PAL_STORAGE_MAX_PAGES"] do
-            local pal_slot = pal_storage:GetSlot(curr_page_idx, curr_slot_idx)
-            if pal_slot:IsEmpty() then
-                curr_slot_idx = curr_slot_idx + 1
-
-                if curr_slot_idx == PalHelpers["PAL_STORAGE_MAX_SLOTS_PER_PAGE"] then
-                    curr_page_idx = curr_page_idx + 1
-                    curr_slot_idx = 0
-                    goto continue
-                end
-            else
-                page_idx = curr_page_idx
-                slot_idx = curr_slot_idx + 1
-
-                -- We add one to these to follow 1-based indexing in Lua
-                return curr_page_idx + 1, curr_slot_idx + 1, pal_slot
-            end
-
-            ::continue::
-        end
-
-        return nil
-    end
-
-    return iterator
-end
-
 ---@param db_char_param UPalDatabaseCharacterParameter
 ---@param pal_char_id FName
 ---@return string
@@ -127,11 +72,12 @@ function PalHelpers.GetAlertDialogControls(world_ctx, pal_util)
     local function show_alert(message)
         if alert_dialog_widget == nil then -- Find alert widget
             local pre_id, post_id
-            pre_id, post_id = RegisterHook("/Game/Pal/Blueprint/UI/Dialog/WBP_PalDialog.WBP_PalDialog_C:OnSetup", function(target_dialog_widget) ---@param target_dialog_widget UWBP_PalDialog_C
-                alert_dialog_widget = target_dialog_widget:get()
-                alert_dialog_widget:SetupUI(alert_dialog_widget.Parameter.DialogType, FText(message))
-                UnregisterHook("/Game/Pal/Blueprint/UI/Dialog/WBP_PalDialog.WBP_PalDialog_C:OnSetup", pre_id, post_id)
-            end)
+            pre_id, post_id = RegisterHook("/Game/Pal/Blueprint/UI/Dialog/WBP_PalDialog.WBP_PalDialog_C:OnSetup",
+                function(target_dialog_widget) ---@param target_dialog_widget UWBP_PalDialog_C
+                    alert_dialog_widget = target_dialog_widget:get()
+                    alert_dialog_widget:SetupUI(alert_dialog_widget.Parameter.DialogType, FText(message))
+                    UnregisterHook("/Game/Pal/Blueprint/UI/Dialog/WBP_PalDialog.WBP_PalDialog_C:OnSetup", pre_id, post_id)
+                end)
             pal_util:Alert(world_ctx, FText(""))
         else
             if not alert_dialog_widget:IsActivated() then
